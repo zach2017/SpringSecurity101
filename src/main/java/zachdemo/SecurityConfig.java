f1e16f78c91b8a7a3ca9b01e37e5a097c9f1a2e4
zacharylewis <zacharylewis@192.168.103.49>
2018-05-02 15:17:06 -0500

    new

--- src/main/java/zachdemo/SecurityConfig.java
+++ src/main/java/zachdemo/SecurityConfig.java
@@ -7,19 +7,61 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfig {
-
 	
+	@Bean
+	public UserDetailsService userDetailsService() throws Exception {
+	// ensure the passwords are encoded properly
+	UserBuilder users = User.withDefaultPasswordEncoder();
+	InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(); 
+	
+	manager.createUser(users.username("user").password("password").roles("USER").build()); 
+	manager.createUser(users.username("admin").password("password").roles("USER","ADMIN").build()); 
+	return manager;
+	}
+	
+	@Configuration
+	@Order(1)
+	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+	
+		protected void configure(HttpSecurity http) throws Exception { 
+		http.antMatcher("/private/**")
+		.authorizeRequests()
+		        .anyRequest().hasRole("ADMIN")
+		        .and()
+		.httpBasic();
+	   }
+	}
+
+	@Configuration
+	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception { 
+			
+			http	.authorizeRequests()
+		        .anyRequest().authenticated()
+		        .and()
+		.formLogin();
+	}
+	} 
+	
+	/*
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     	
@@ -37,7 +79,7 @@ public class SecurityConfig {
                   // .anyRequest().hasRole("USER");
         }
     } 
-    
+    */
 
     //    @Autowired
 	//public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
