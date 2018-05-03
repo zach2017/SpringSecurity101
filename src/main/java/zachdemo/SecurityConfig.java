da1a20abf15e76d6aae0645207fcc09c7cd1d20c
zacharylewis <zacharylewis@192.168.103.48>
2018-05-03 12:03:45 -0500

    k

--- src/main/java/zachdemo/SecurityConfig.java
+++ src/main/java/zachdemo/SecurityConfig.java
@@ -1,90 +1,54 @@
 package zachdemo;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.AuthenticationEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfig {
-	
+
 	@Bean
-	public UserDetailsService userDetailsService() throws Exception {
-	// ensure the passwords are encoded properly
-	UserBuilder users = User.withDefaultPasswordEncoder();
-	InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(); 
-	
-	manager.createUser(users.username("user").password("password").roles("USER").build()); 
-	manager.createUser(users.username("admin").password("password").roles("USER","ADMIN").build()); 
-	return manager;
+	public UserDetailsService userDetailsService() {
+		
+		// ensure the passwords are encoded properly
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+		manager.createUser(users.username("user").password("password").roles("USER").build());
+		manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+
+		return manager;
+
 	}
-	
+
 	@Configuration
 	@Order(1)
 	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-	
-		protected void configure(HttpSecurity http) throws Exception { 
-		http.antMatcher("/private/**")
-		.authorizeRequests()
-		        .anyRequest().hasRole("ADMIN")
-		        .and()
-		.httpBasic();
-	   }
+
+		protected void configure(HttpSecurity http) throws Exception {
+
+			http.antMatcher("/public/**").authorizeRequests().anyRequest().hasRole("USER").and().httpBasic();
+
+			http.antMatcher("/private/**").authorizeRequests().anyRequest().hasRole("ADMIN").and().httpBasic();
+
+		}
 	}
 
 	@Configuration
 	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-		
-		@Override
-		protected void configure(HttpSecurity http) throws Exception { 
-			
-			http	.authorizeRequests()
-		        .anyRequest().authenticated()
-		        .and()
-		.formLogin();
-	}
-	} 
-	
-	/*
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    	
-        auth
-            .inMemoryAuthentication()
-            .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser("user").password("password").roles("USER");
-    }
-    
-    @Configuration 
-    @Order(1)
-    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers("/private/**").denyAll();
-                  // .anyRequest().hasRole("USER");
-        }
-    } 
-    */
 
-    //    @Autowired
-	//public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-    //        request.getRequestDispatcher("/spring/denied").forward(request, response);
-      //  }
-   // 
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+
+			http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+		}
+	}
 
 }
--- src/main/webapp/private/private.jsp
+++ src/main/webapp/private/private.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="sec"
+	uri="http://www.springframework.org/security/tags"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,17 +9,21 @@
 <title>private JSP Page</title>
 </head>
 <body>
-  <div class="container">
-    <h1>This is secured!</h1>
-    <p>
-    <%= request.getRequestURI() %>
-      Hello <b>name:<c:out value="${pageContext.request.remoteUser}"/></b>
-    </p>
-    <c:url var="logoutUrl" value="/logout"/>
-    <form class="form-inline" action="${logoutUrl}" method="post">
-      <input type="submit" value="Log out" />
-      <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
-    </form>
-  </div>
+	<div class="container">
+		<h1>This Should be Admin Only secured!</h1>
+		<p>Hello
+		<P>
+			name:
+			<%=request.getUserPrincipal().getName()%>
+		</p>
+		<hr>
+		Roles <b>Role: <%=request.getUserPrincipal().toString()%></b>
+		</p>
+		Return
+		<UL>
+			<LI><a href="../private/">Private Page</a></LI>
+			<LI><a href="../public/">Public Page</a></LI>
+		</UL>
+	</div>
 </body>
 </html>
